1f8f47b280421c226e916aa55ab71b27a1572ebe
unknown <roudrini.m@utahtechlab.com>
2025-01-03 12:00:12 +0530

    first commit

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -6,6 +6,7 @@ import { useNavigate } from 'react-router-dom';
 
 const LoginPage = () => {
   const dispatch = useDispatch();
+  
   const navigate = useNavigate();
 
   const handleLogin = () => {
